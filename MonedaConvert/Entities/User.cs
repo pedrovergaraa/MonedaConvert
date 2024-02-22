e7c118faf52469526ec49b8810754ff117f57c61
Pedro Vergara <pvergara2407@gmail.com>
2024-02-22 08:00:43 -0300

    Carpetas creadas

--- MonedaConvert/Entities/User.cs
+++ MonedaConvert/Entities/User.cs
@@ -1,6 +1,14 @@
-﻿namespace MonedaConvert.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MonedaConvert.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
     }
 }
